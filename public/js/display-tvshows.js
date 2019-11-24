560ca32db6288ad52e514c609ed7be750fa046da
thusharabandara <thushararanjana@gmail.com>
2019-11-24 08:50:51 +0530

    create display-tvshows.js, in order to display tvshows.xml data

---
+++ public/js/display-tvshows.js
@@ -0,0 +1,22 @@
+function loadTvShows() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        displayTvShows(this);
+      }
+    };
+    xmlhttp.open("GET", "/public/xml/tvshows.xml", true);
+    xmlhttp.send();
+  }
+  function displayTvShows(xml) {
+    var i;
+    var xmlDoc = xml.responseXML;
+    var tvshowInfo = "";
+    var x = xmlDoc.getElementsByTagName("tvshow");
+    for (i = 0; i <x.length; i++) {
+        tvshowInfo += "<ul><li>" +
+        x[i].getElementsByTagName("tvshow-title")[0].childNodes[0].nodeValue +
+        "</li></ul>";
+    }
+    document.getElementById("tvshow-info").innerHTML = tvshowInfo;
+  }
