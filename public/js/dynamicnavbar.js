fceb5ffcc2b492f583084414942d6ec64e35211e
thusharabandara <thushararanjana@gmail.com>
2019-12-16 18:39:03 +0530

    implement a new function to handle the navbars in interest pages

--- public/js/dynamicnavbar.js
+++ public/js/dynamicnavbar.js
@@ -3,3 +3,9 @@ function dynamicNav() {
         $('nav').toggleClass('scrolled', $(this).scrollTop() > 620);
     });
 };
+
+function dynamicNavInterest() {
+    $(window).scroll(function () {
+        $('nav').toggleClass('scrolled', $(this).scrollTop() > 10);
+    });
+};
