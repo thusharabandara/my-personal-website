3140cb04bf8510b766441a7b8709191814d331b3
thusharabandara <thushararanjana@gmail.com>
2020-01-22 19:57:13 +0530

    fix file not found problem

--- public/js/displayhomeposts.js
+++ public/js/displayhomeposts.js
@@ -21,10 +21,10 @@ function displayHomePosts(xml) {
         if (posts[i].getAttribute('home') == "1") {
             homePostInfo += "<div class=\"card\" style=\"width: 18rem;\"><div class=\"card-body\"><h5 class=\"card-title\">" +
                 posts[i].getElementsByTagName("post-title")[0].childNodes[0].nodeValue + "</h5><h6 class=\"card-subtitle mb-2 text-muted\">" +
-                posts[i].getElementsByTagName("author-name")[0].childNodes[0].nodeValue + "</h6><p class=\"card-text text-muted\" target=\"_blank\">" +
+                posts[i].getElementsByTagName("author-name")[0].childNodes[0].nodeValue + "</h6><p class=\"card-text text-muted\">" +
                 posts[i].getElementsByTagName("posted-date")[0].childNodes[0].nodeValue + "</p><a href=\"" +
                 posts[i].getElementsByTagName("post-path")[0].childNodes[0].nodeValue +
-                "\ class=\"card-link\">Read Post</a></div></div>";
+                "\" target=\"_blank\" class=\"card-link\">Read Post</a></div></div>";
         }
     }
     // Pass the homePostInfo to HTML, in order to display the content
